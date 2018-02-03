8ef28d6a43a8cff5148261a39b97e09f568ed971
isaac1104 <isaac-kwon@hotmail.com>
2018-02-03 11:21:24 -0800

    buttons added as placeholders

--- client/src/components/ProjectListItem.js
+++ client/src/components/ProjectListItem.js
@@ -3,7 +3,7 @@ import React from "react";
 const ProjectListItem = (props) => {
   return (
     <li className="list-group-item d-flex justify-content-between align-items-center">
-      <input type="radio"/>
+      <input type="radio" style={{marginLeft: "10px"}}/>
       <h6>Project List Item with Radio Button, Edit & View Map Button 1</h6>
       <div className="d-flex">
         <button className="btn btn-sm btn-primary">EDIT</button>
