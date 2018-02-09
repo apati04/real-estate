5fb559de936014216edd4113db44257ca2558ef4
Andrew Patipaksiri <apax714@gmail.com>
2018-02-08 19:58:16 -0800

    clean up comments

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,11 +4,6 @@ import convert from "xml-js";
 import { FETCH_CURRENT_USER_DATA, FETCH_PROPERTY_DATA, FETCH_MAP_DATA } from "./types";
 import keys from '../config/keys';
 
-// const ZILLOW_URL = "https://www.zillow.com/webservice/GetDeepSearchResults.htm";
-// const ZILLOW_KEY = "X1-ZWz18sa4dxcwln_8jmj2";
-//
-// const MAPBOX_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places";
-// const MAPBOX_TOKEN = "pk.eyJ1IjoiaXNhYWMxMTA0IiwiYSI6ImNqZDgwYjJ5MTI1dXUycWw5M3E5bnpldDcifQ.tRpvJ9X5wq7ke4t9KGd4yg";
 
 export const fetchCurrentUserData = () => async dispatch => {
   const request = await axios.get("/api/current_user");
