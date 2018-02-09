7e2bf8ef71315c7b1d558991cd39ff6dd24d62e6
Andrew Patipaksiri <apax714@gmail.com>
2018-02-08 19:56:54 -0800

    add protected middleware and config folder for api keys

---
+++ client/src/config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  zillowUrl: 'https://www.zillow.com/webservice/GetDeepSearchResults.htm',
+  mapboxUrl: 'https://api.mapbox.com/geocoding/v5/mapbox.places',
+  zillowKey: 'X1-ZWz18sa4dxcwln_8jmj2',
+  mapboxToken: 'pk.eyJ1IjoiaXNhYWMxMTA0IiwiYSI6ImNqZDgwYjJ5MTI1dXUycWw5M3E5bnpldDcifQ.tRpvJ9X5wq7ke4t9KGd4yg'
+}
