19b5aac423c2078c2f054b18a23291f399302713
isaac1104 <isaac-kwon@hotmail.com>
2018-03-19 21:20:07 -0700

    logout redirect auth change

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "../containers/Login";
 import Signup from "../containers/Signup";
 import Projects from "../containers/Projects";
 import Map from "../containers/Map";
 import Dashboard from "../components/Dashboard";
-import { connect } from 'react-redux';
-import * as actions from '../actions';
 import Navbar from '../containers/Navbar';
 import Sidebar from './Sidebar';
+import Landing from './Landing';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 class App extends Component {
   renderSidebar() {
     if (this.props.currentUser) {
@@ -23,16 +24,19 @@ class App extends Component {
       <BrowserRouter>
         <div>
           <Navbar/>
-          <Route exact path="/" component={Login}/>
-          <div id="wrapper" className="toggled">
-            {this.renderSidebar()}
-            <div id="page-content-wrapper">
-              <Route path="/map" component={Map}/>
-              <Route path="/projects" component={Projects}/>
-              <Route path="/signup" component={Signup}/>
-              <Route path ="/dashboard" component={Dashboard}/>
+          <Switch>
+            <Route path="/login" component={Login}/>
+            <div id="wrapper" className="toggled">
+              {this.renderSidebar()}
+              <div id="page-content-wrapper">
+                <Route path ="/dashboard" component={Dashboard}/>
+                <Route path="/map" component={Map}/>
+                <Route path="/projects" component={Projects}/>
+                <Route path="/signup" component={Signup}/>
+              </div>
             </div>
-          </div>
+            <Route exact path="/" component={Landing}/>
+          </Switch>
         </div>
       </BrowserRouter>
     );
