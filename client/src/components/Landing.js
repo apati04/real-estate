39f6c084b702f776fa1ca1d632fda69ff31b3ff0
isaac1104 <isaac-kwon@hotmail.com>
2018-03-21 22:14:20 -0700

    landing

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import VideoCarousel from './VideoCarousel';
+import { View, Mask, Button } from 'mdbreact';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
-    <div>
+    <View className="hm-black-strong">
       <VideoCarousel/>
-    </div>
+      <Mask className="d-flex full-bg-img flex-center ">
+        <div className="d-flex container justify-content-around p-0 text-left white-text wow fadeInUp">
+          <div className="text-center">
+            <h2 className="mb-4 h2-responsive">Welcome to Real Estate Manager</h2>
+            <h3 className="h3-responsive">Your Dream Meets Reality Here</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In mollis libero orci, in venenatis lectus gravida condimentum. Nullam pretium urna ut lacinia gravida. Proin pellentesque, est eu posuere suscipit, erat neque ultricies erat, non tempor justo sem blandit massa. Cras a nulla scelerisque lacus rutrum vulputate. Integer imperdiet, massa non.</p>
+            <Link to="/login"><Button>LOGIN</Button></Link>
+          </div>
+        </div>
+      </Mask>
+    </View>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,4 @@
 import $ from 'jquery';
-import 'font-awesome/css/font-awesome.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
-import '../node_modules/react-vis/dist/style.css';
-import 'bootstrap-material-design/dist/css/bootstrap-material-design.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
@@ -14,6 +9,12 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 import './styles/sidebar.css';
 import './styles/navbar.css';
+import './styles/index.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import '../node_modules/react-vis/dist/style.css';
+import 'bootstrap-material-design/dist/css/bootstrap-material-design.min.css';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
