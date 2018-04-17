e483ed9b50087acbcc52b9c558fcc1423874e995
isaac1104 <isaac-kwon@hotmail.com>
2018-04-16 19:13:08 -0700

    forms added

--- client/src/components/NewProject.js
+++ client/src/components/NewProject.js
@@ -1,11 +1,69 @@
 import React, { Component } from 'react';
+import ContentLayout from './layout/ContentLayout';
+import FormField from './forms/FormField';
+import { Field, reduxForm } from "redux-form"
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class NewProject extends Component {
+  componentDidMount() {
+    this.props.fetchCurrentUserData();
+  }
+
+  formSubmit = value => {
+    console.log(value);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <h1>Fomr</h1>
+      <ContentLayout>
+        <form onSubmit={handleSubmit(this.formSubmit)}>
+          <Field
+            label="ADDRESS"
+            name="address"
+            component={FormField}
+          />
+          <div className='row'>
+            <div className='col-md-6'>
+              <Field
+                label="Longitude"
+                name="longitude"
+                component={FormField}
+              />
+              <Field
+                label="Latitude"
+                name="latitude"
+                component={FormField}
+              />
+            </div>
+            <div className='col-md-6'>
+              <Field
+                label="Color RGB"
+                name="color"
+                component={FormField}
+              />
+              <Field
+                label="Placemark Height"
+                name="placemark"
+                component={FormField}
+              />
+            </div>
+          </div>
+          <button className="btn btn-raised btn-info" type="submit">SAVE PROPERTY</button>
+        </form>
+      </ContentLayout>
     );
   }
 }
 
-export default NewProject;
+function validate(values) {
+  const errors = {};
+  if (!values.address) {
+    errors.address = 'Please enter the address';
+  }
+  return errors;
+}
+
+export default reduxForm({ form: 'propDetail', validate })(connect(null, actions)(NewProject));
--- client/src/components/ProjectList.js
+++ client/src/components/ProjectList.js
@@ -9,10 +9,10 @@ const CurrentProjects = () => {
         {/* map through database to display projectlistItem */}
         <ProjectListItem/>
       </ul>
-      <button className="btn btn-raised btn-primary" style={{ marginTop: "20px" }}>ADD NEW PROJECT</button>
+      <Link to='/project/new' className="btn btn-raised btn-primary" style={{ marginTop: "20px" }}>ADD NEW PROJECT</Link>
       <hr/>
       <div className="d-flex justify-content-between">
-        <Link to='/project/new' className="btn btn-raised btn-success">SAVE PROJECT</Link>
+        <button className="btn btn-raised btn-success">SAVE PROJECT</button>
         <button className="btn btn-raised btn-danger">DELETE SELECTED PROJECTS</button>
       </div>
     </div>
