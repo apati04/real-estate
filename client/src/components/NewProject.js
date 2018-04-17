7e9f1f319e5b14c933c8137920334e3b3de4772e
isaac1104 <isaac-kwon@hotmail.com>
2018-04-16 18:58:39 -0700

    new route added

---
+++ client/src/components/NewProject.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NewProject extends Component {
+  render() {
+    return (
+      <h1>Fomr</h1>
+    );
+  }
+}
+
+export default NewProject;
--- client/src/components/ProjectList.js
+++ client/src/components/ProjectList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ProjectListItem from "./ProjectListItem";
+import { Link } from 'react-router-dom';
 
 const CurrentProjects = () => {
   return (
@@ -11,7 +12,7 @@ const CurrentProjects = () => {
       <button className="btn btn-raised btn-primary" style={{ marginTop: "20px" }}>ADD NEW PROJECT</button>
       <hr/>
       <div className="d-flex justify-content-between">
-        <button className="btn btn-raised btn-success">SAVE PROJECT</button>
+        <Link to='/project/new' className="btn btn-raised btn-success">SAVE PROJECT</Link>
         <button className="btn btn-raised btn-danger">DELETE SELECTED PROJECTS</button>
       </div>
     </div>
