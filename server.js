ce6b2cc57c6667c1962ad9df3996e25d6d675859
Andrew Patipaksiri <apax714@gmail.com>
2018-04-17 18:07:29 -0700

    change building model name and create post route

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 const PORT = process.env.PORT || 5000;
 
@@ -15,11 +14,11 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [ keys.cookieKey ]
+    keys: [keys.cookieKey]
   })
 );
 app.use(passport.initialize());
