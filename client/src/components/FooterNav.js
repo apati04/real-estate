14b999c69adb14d1f0c5a68b887e51a6f74d8271
isaac1104 <isaac-kwon@hotmail.com>
2018-05-05 16:49:52 -0700

    footer added

---
+++ client/src/components/FooterNav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Layout } from 'antd';
+const { Footer } = Layout;
+
+const FooterNav = () => {
+  return (
+    <Footer style={{ textAlign: 'center' }}>
+      Ant Design ©2016 Created by Ant UED
+    </Footer>
+  );
+}
+
+export default FooterNav;
