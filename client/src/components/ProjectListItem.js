dee6060c7f37221db4221615b4bb5bdde8a0d888
isaac1104 <isaac-kwon@hotmail.com>
2018-05-05 19:02:48 -0700

    btn icons added

--- client/src/components/ProjectListItem.js
+++ client/src/components/ProjectListItem.js
@@ -11,8 +11,8 @@ class ProjectListItem extends Component {
   render() {
     return (
       <div>
-        <li className="list-group-item d-flex justify-content-between align-items-center">
-          <input type="radio" style={{marginLeft: "10px"}} name="option"/>
+        <li className="d-flex justify-content-between align-items-center">
+          <input type="radio" style={{ marginLeft: "10px" }} name="option"/>
           <h6>Project List Item with Radio Button, Edit & View Map Button 1</h6>
           <div className="d-flex">
             <Link to='/projects/edit' className="btn btn-sm btn-raised btn-primary">
