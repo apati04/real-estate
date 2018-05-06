98f559f8ed8e417f34793fda9e74cd0a9d06d3a6
Andrew Patipaksiri <apax714@gmail.com>
2018-05-05 17:00:29 -0700

    build

---
+++ config/prod.js
@@ -0,0 +1,18 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+
+/**
+ *   googleClientID:
+    '216959651031-bf5lng1sk2ehu7a83hefbrvl5qapr7dr.apps.googleusercontent.com',
+  googleClientSecret: 'PP3gyjXkDGNMH6KMzJN18y4k',
+  mongoURI: 'mongodb://root:root@ds121248.mlab.com:21248/realestate-dev',
+  twitterClientID: 'z0IQOriYmwsEInXhUnP6yZ1dB',
+  twitterClientSecret: 'MYH4gsS6bCf19OLwzXPrlFxJ8Cbmzk9cBNJbX8lMjnFRJwP2FB',
+  cookieKey: 'badsfasdf',
+  facebookClientID: '87422aadacb26091cf33005c9cbeeddc',
+  facebookClientSecret: '1052065824933530'
+ */
