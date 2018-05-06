91d787c7d3ee46c640549981283a019351bebf24
Andrew Patipaksiri <apax714@gmail.com>
2018-05-06 14:23:32 -0700

    fix delete building functionality

--- client/src/reducers/user_properties_reducer.js
+++ client/src/reducers/user_properties_reducer.js
@@ -1,9 +1,19 @@
-import { FETCH_PROPERTIES } from '../actions/types';
-
-export default function(state = [], action) {
+import { FETCH_PROPERTIES, DELETE_SELECTED_PROPERTY } from '../actions/types';
+const initialState = {
+  buildings: []
+};
+export default function(state = initialState, action) {
   switch (action.type) {
-    case FETCH_PROPERTIES:
-      return action.payload;
+    case FETCH_PROPERTIES: {
+      return { ...state, buildings: action.payload };
+    }
+    case DELETE_SELECTED_PROPERTY: {
+      const { _id } = action.payload;
+      return {
+        ...state,
+        buildings: state.buildings.filter(item => item._id !== _id)
+      };
+    }
     default:
       return state;
   }
