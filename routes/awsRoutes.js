4bde6ed2cde3b48ba0d4579516cb100af879ff0f
Andrew Patipaksiri <apax714@gmail.com>
2018-05-07 20:17:50 -0700

    aws route setup

--- routes/awsRoutes.js
+++ routes/awsRoutes.js
@@ -1,3 +1,11 @@
 const uuid = require('uuid/v1');
+const AWS = require('aws-sdk');
 const requireLogin = require('../middlewares/requireAuth');
 const keys = require('../config/keys');
+
+// presigned url
+const s3 = new AWS.S3({
+  accessKeyId: keys.accessKeyId,
+  secretAccessKey: keys.secretAccessKey
+});
+module.exports = app => {};
