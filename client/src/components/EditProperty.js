29171f90bc5487e63a67da8a14adc00c5d1ae9a5
isaac1104 <isaac-kwon@hotmail.com>
2018-05-07 22:23:06 -0700

    fetching current building data in edit page

--- client/src/components/EditProperty.js
+++ client/src/components/EditProperty.js
@@ -1,20 +1,49 @@
 import React, { Component } from 'react';
 import ContentLayout from './layout/ContentLayout';
+import { Link } from 'react-router-dom';
 import * as actions from '../actions';
 import { connect } from 'react-redux';
 
 class EditProperty extends Component {
   componentDidMount() {
     this.props.fetchCurrentUserData();
+    this.props.fetchProperties();
   }
 
   render() {
+    const { id } = this.props.match.params;
+    const { buildings } = this.props.userProperties;
+    const currentBuilding = buildings.filter(building => building._id === id);
+
     return (
       <ContentLayout>
         <h1>Edit Property</h1>
+        {currentBuilding.map(building => {
+          return (
+            <div>
+              <h3>ID: {building._id}</h3>
+              <h3>Address: {building.address}</h3>
+              <h3>Longitude: {building.longitude}</h3>
+              <h3>Latitude: {building.latitude}</h3>
+              <h3>Color: {building.color}</h3>
+              <h3>Placemark: {building.placemark}</h3>
+            </div>
+          )
+        })}
+        <div className='float-right'>
+          <Link to="/projects/edit" className="btn btn-raised btn-danger">
+            <i className="fas fa-undo" /> BACK
+          </Link>
+        </div>
       </ContentLayout>
     );
   }
 }
 
-export default connect(null, actions)(EditProperty);
+function mapStateToProps({ userProperties }) {
+  return {
+    userProperties
+  }
+}
+
+export default connect(mapStateToProps, actions)(EditProperty);
--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ContentLayout from './layout/ContentLayout';
+import { Link } from 'react-router-dom';
 import { fetchCurrentUserData } from '../actions';
 import { connect } from 'react-redux';
 
@@ -12,7 +13,11 @@ class NotFound extends Component {
   render() {
     return (
       <ContentLayout>
+        <div id='mapbox'/>
         <h1>PAGE NOT FOUND</h1>
+        <Link to="/projects" className="btn btn-raised btn-danger">
+          <i className="fas fa-undo" /> BACK
+        </Link>
       </ContentLayout>
     );
   }
