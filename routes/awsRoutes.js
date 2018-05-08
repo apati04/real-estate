ab559bf47533c0ce5283fd2fa98e5ccfde4d6236
Andrew Patipaksiri <apax714@gmail.com>
2018-05-07 20:29:48 -0700

    get presigned url

--- routes/awsRoutes.js
+++ routes/awsRoutes.js
@@ -1,6 +1,6 @@
 const uuid = require('uuid/v1');
 const AWS = require('aws-sdk');
-const requireLogin = require('../middlewares/requireAuth');
+const requireAuth = require('../middlewares/requireAuth');
 const keys = require('../config/keys');
 
 // presigned url
@@ -8,4 +8,17 @@ const s3 = new AWS.S3({
   accessKeyId: keys.accessKeyId,
   secretAccessKey: keys.secretAccessKey
 });
-module.exports = app => {};
+module.exports = app => {
+  app.get('/api/awsUpload', (req, res) => {
+    const uniqueKey = `${req.user.id}/${uuid()}.jpeg`;
+
+    const bucketObj = {
+      Bucket: 'rem-bucket-9818',
+      ContentType: 'image/jpeg',
+      Key: uniqueKey
+    };
+    s3.getSignedUrl('putObject', bucketObj, (err, url) =>
+      res.send({ uniqueKey, url })
+    );
+  });
+};
