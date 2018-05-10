dd8c84f920f7e2ac99280713f33d386a8fab837b
Andrew Patipaksiri <apax714@gmail.com>
2018-05-09 23:28:51 -0700

    add project

--- client/src/components/projects/ProjectForm/ProjectForm.js
+++ client/src/components/projects/ProjectForm/ProjectForm.js
@@ -1,64 +1,87 @@
-import React from 'react';
-import { Field, FieldArray, reduxForm } from 'redux-form';
-import { Button } from 'antd';
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { TextField } from 'redux-form-antd';
+import { Button, Modal, Form } from 'antd';
 import ProjectField from './ProjectField';
+const FormItem = Form.Item;
+const ProjectForm = Form.create()(
+  class extends Component {
+    render() {
+      const { visible, onCancel, onCreate, form, handleSubmit } = this.props;
 
-const renderProjectForm = ({
-  submitting,
-  pristine,
-  reset,
-  fields,
-  meta: { touched, error, submitFailed }
-}) => (
-  <div>
-    <button
-      type="button"
-      className="btn btn-raised btn-primary"
-      style={{ marginTop: '20px' }}
-      onClick={() => fields.push({})}
-    >
-      <i className="fas fa-plus-circle" /> ADD NEW PROJECT
-    </button>
-    {(touched || submitFailed) && error && <span>{error}</span>}
-
-    <ul className="list-unstyled">
-      {fields.map((item, index) => (
-        <li key={index}>
-          <h4>New Project</h4>
-          <Field
-            name={`${item}.title`}
-            type="text"
-            component={ProjectField}
-            label="Project Name"
-          />
-          <Field
-            name={`${item}.description`}
-            type="text"
-            component={ProjectField}
-            label="Project Description"
-          />
-          <div>
-            <button
-              className="btn btn-primary"
-              type="submit"
-              disabled={submitting}
-            >
-              Create
-            </button>
-          </div>
-        </li>
-      ))}
-    </ul>
-  </div>
+      return (
+        <Form onSubmit={handleSubmit}>
+          <Modal
+            visible={visible}
+            title="Create a new project"
+            okText="Create"
+            onCancel={onCancel}
+            onOk={handleSubmit}
+          >
+            <Field
+              label="Title"
+              name="projectTitle"
+              type="text"
+              component={TextField}
+            />
+            <Field
+              name="projectDescription"
+              type="text"
+              component={TextField}
+              label="Description"
+            />
+          </Modal>
+        </Form>
+      );
+    }
+  }
 );
-const ProjectForm = ({ handleSubmit, pristine, reset, submitting }) => {
-  return (
-    <form onSubmit={handleSubmit}>
-      <FieldArray name="projects" component={renderProjectForm} />
-    </form>
-  );
-};
-
+// const renderProjectForm = ({
+//   submitting,
+//   pristine,
+//   reset,
+//   fields,
+//   meta: { touched, error, submitFailed }
+// }) => (
+//   <div>
+//     <ul className="list-unstyled">
+//       {fields.map((item, index) => (
+//         <li key={index}>
+//           <h4>New Project</h4>
+//         </li>
+//       ))}
+//     </ul>
+//   </div>
+// );
+// class ProjectForm extends Component {
+//   render() {
+//     const {
+//       submitting,
+//       pristine,
+//       reset,
+//       fields,
+//       visible,
+//       onCancel,
+//       onCreate,
+//       handleSubmit
+//     } = this.props;
+//   }
+// }
+const formFields = [
+  { label: 'Title', name: 'projectTitle' },
+  { label: 'Description', name: 'projectDescription' }
+];
+function validate(values) {
+  const errors = {};
+  _.each(formFields, ({ name }) => {
+    if (!values[name]) {
+      errors[name] = 'Enter a Value';
+    }
+  });
+  return errors;
+}
 export default reduxForm({
+  validate,
   form: 'projectForm'
 })(ProjectForm);
