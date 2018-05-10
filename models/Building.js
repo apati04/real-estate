91f1027e59ee62c5ffe2879df4ea43130685de4f
Andrew Patipaksiri <apax714@gmail.com>
2018-05-10 12:52:20 -0700

    add project logic

--- models/Building.js
+++ models/Building.js
@@ -16,7 +16,7 @@ const buildingSchema = new Schema({
   certifications: String,
   prevOwner: String,
   dateAdded: Date,
-  _projectId: { type: Schema.Types.ObjectId, ref: 'Project' }
+  _project: { type: Schema.Types.ObjectId, ref: 'Project' }
 });
 
 mongoose.model('Building', buildingSchema);
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const requireAuth = require('../middlewares/requireAuth');
 
 const Project = mongoose.model('Project');
+const Building = mongoose.model('Building');
 
 module.exports = app => {
   app.get('/api/projects', requireAuth, async (req, res, next) => {
@@ -30,6 +31,21 @@ module.exports = app => {
     try {
       await project.save();
       res.send(project);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  });
+  app.post('/api/projects/:_id', async (req, res, next) => {
+    const { address, owner, built } = req.body;
+    const building = new Building({
+      address,
+      owner,
+      built,
+      _project: req.params._id
+    });
+    try {
+      await building.save();
+      res.send(building);
     } catch (error) {
       res.status(400).send(error);
     }
