5b45d7703f75269e8dc981f995e05170c84dbb2c
Andrew Patipaksiri <apax714@gmail.com>
2018-05-12 12:15:55 -0700

    user settings routes

---
+++ client/src/components/users/UserSettings.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ContentLayout from '../layout/ContentLayout';
+
+class UserSettings extends Component {
+  render() {
+    return <ContentLayout>UserSettings</ContentLayout>;
+  }
+}
+
+export default UserSettings;
