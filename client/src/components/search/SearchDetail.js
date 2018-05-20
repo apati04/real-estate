0370fc593a3a8befe8b4f3671c7ed10e64d0cbd8
isaac1104 <isaac-kwon@hotmail.com>
2018-05-19 22:22:24 -0700

    refactor

--- client/src/components/search/SearchDetail.js
+++ client/src/components/search/SearchDetail.js
@@ -10,6 +10,7 @@ class SearchDetail extends Component {
     this.setState({ [type]: key });
   };
   render() {
+    const { fullAddress, address, type, yearBuilt, rooms, lotSize, financials } = this.props;
     console.log(this.props);
     const tabList = [
       {
@@ -30,33 +31,33 @@ class SearchDetail extends Component {
       Location: (
         <Fragment>
           <p className='card-title'>Address</p>
-          <p className='card-text'>{this.props.fullAddress}</p>
+          <p className='card-text'>{fullAddress}</p>
           <p className='card-title'>Longitude</p>
-          <p className='card-text'>{this.props.address.longitude}</p>
+          <p className='card-text'>{address.longitude}</p>
           <p className='card-title'>Latitude</p>
-          <p className='card-text'>{this.props.address.latitude}</p>
+          <p className='card-text'>{address.latitude}</p>
         </Fragment>
       ),
       About: (
         <Fragment>
           <p className='card-title'>Type</p>
-          <p className='card-text'>{this.props.type}</p>
+          <p className='card-text'>{type}</p>
           <p className='card-title'>Year Built</p>
-          <p className='card-text'>{this.props.yearBuilt}</p>
+          <p className='card-text'>{yearBuilt}</p>
           <p className='card-title'>Bedrooms</p>
-          <p className='card-text'>{this.props.rooms.bedrooms}</p>
+          <p className='card-text'>{rooms.bedrooms}</p>
           <p className='card-title'>Bathrooms</p>
-          <p className='card-text'>{this.props.rooms.bathrooms}</p>
+          <p className='card-text'>{rooms.bathrooms}</p>
           <p className='card-title'>Lot Size</p>
-          <p className='card-text'>{this.props.lotSize.value} SqFt</p>
+          <p className='card-text'>{lotSize.value} SqFt</p>
         </Fragment>
       ),
       TaxAssessment: (
         <Fragment>
           <p className='card-title'>Tax Year</p>
-          <p className='card-text'>{this.props.financials ? this.props.financials.taxAssessment.year : 'N/A'}</p>
+          <p className='card-text'>{financials ? financials.taxAssessment.year : 'N/A'}</p>
           <p className='card-title'>Assessment Amount</p>
-          <p className='card-text'>{this.props.financials ? `$${Math.round(this.props.financials.taxAssessment.amount).toLocaleString()}` : 'N/A'}</p>
+          <p className='card-text'>{financials ? `$${Math.round(financials.taxAssessment.amount).toLocaleString()}` : 'N/A'}</p>
         </Fragment>
       )
     };
