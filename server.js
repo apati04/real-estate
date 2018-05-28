2b85db9801544774675b978a27360e5560790c9d
Andrew Patipaksiri <apax714@gmail.com>
2018-05-28 16:15:15 -0700

    remove cookies and install dependencies for jwts

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
 const passport = require('passport');
+const router = require('./routes/router');
 const keys = require('./config/keys');
 const PORT = process.env.PORT || 5000;
 
@@ -14,24 +14,10 @@ mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI);
 
 const app = express();
-
-app.use(express.json());
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-
-require('./routes/auth')(app);
-require('./routes/twitter')(app);
-require('./routes/buildingRoutes')(app);
-require('./routes/projectRoutes')(app);
-require('./routes/awsRoutes')(app);
-require('./routes/apiRoutes')(app);
-
+app.use(morgan('combined'));
+app.use(cors());
+app.use(express.json({ type: '*/*' }));
+router(app);
 app.use((err, req, res, next) => {
   res.status(422).send({ error: err.message });
 });
