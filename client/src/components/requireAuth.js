4f80766897161969ad39433b3d9b966f410f2e23
Andrew Patipaksiri <apax714@gmail.com>
2018-06-02 16:19:47 -0700

    require auth hoc functionality

--- client/src/components/requireAuth.js
+++ client/src/components/requireAuth.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const requireAuth = (WrappedComponent) => {
+const requireAuth = WrappedComponent => {
   class ComposedComponent extends Component {
-
+    componentDidMount() {
+      this.redirectUser();
+    }
+    componentDidUpdate() {
+      this.redirectUser();
+    }
+    redirectUser() {
+      if (!this.props.currentUser) {
+        this.props.history.push('/');
+      }
+    }
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
   }
-  return (
-    <WrappedComponent {...this.props} />
-  );
+  function mapStateToProps({ currentUser }) {
+    return { auth: currentUser };
+  }
+  return connect(mapStateToProps)(ComposedComponent);
 };
-
 export default requireAuth;
